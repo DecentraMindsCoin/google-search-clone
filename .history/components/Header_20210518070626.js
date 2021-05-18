75ce51631e1fc895391d957825101272f4da9314
DecentraMindsCoin <decentraminds@gmail.com>
2021-05-18 07:08:00 -0700

    bug fix

--- .history/components/Header_20210518070626.js
+++ .history/components/Header_20210518070626.js
@@ -30,7 +30,7 @@ function Header() {
             onClick={() => router.push("/")}
             className="cursor-pointer" />
             <form className='px-6 py-3 ml-10 mr-5 flex-grow flex border border-gray-200 rounded-full shadow-lg '>
-                <input ref={searchInputRef} type='text' className='flex-grow w-full focus:outline-none' type='text' defaultValue={router.query.term} />
+                <input ref={searchInputRef} type='text' className='flex-grow w-full focus:outline-none' type='text' defaultValue />
                 <XIcon className='h-7 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125'
                 onClick={() =>(searchInputRef.current.value = "")}/>
                 <MicrophoneIcon className='mr-3 h-6 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-300' />
